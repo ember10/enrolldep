886d06a7a8c5a5d0b1c9345221bebc069e40cc01
ember10 <62350427+ember10@users.noreply.github.com>
2020-03-18 13:01:31 -0500

    Create enrolldep.sh

---
+++ enrolldep.sh
@@ -0,0 +1,9 @@
+CD "/Volumes/Macintosh HD/usr/local/etc"
+rm fwcld.plist
+rm fwcld.newprefs.plist
+Cd “/Volumes/Macintosh HD/var/db/ConfigurationProfiles”
+rm -rf *
+mkdir Settings
+touch Settings/.profilesAreInstalled
+Cd “/Volumes/Macintosh HD/var/db”
+rm .AppleSetupDone
