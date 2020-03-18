53bfcb7247051768a3080000ce2fde7d43a806f1
ember10 <62350427+ember10@users.noreply.github.com>
2020-03-18 13:23:50 -0500

    Update enrolldep.sh

--- enrolldep.sh
+++ enrolldep.sh
@@ -1,10 +1,10 @@
-CD "/Volumes/Macintosh HD/usr/local/etc"
+cd "/Volumes/Macintosh HD/usr/local/etc"
 rm fwcld.plist
 rm fwcld.newprefs.plist
-Cd “/Volumes/Macintosh HD/var/db/ConfigurationProfiles”
+cd “/Volumes/Macintosh HD/var/db/ConfigurationProfiles”
 rm -rf *
 mkdir Settings
 touch Settings/.profilesAreInstalled
-Cd “/Volumes/Macintosh HD/var/db”
+cd “/Volumes/Macintosh HD/var/db”
 rm .AppleSetupDone
 echo "it worked"
