237baab28c16cd871f2c1828ddd08f61ab381091
ember10 <62350427+ember10@users.noreply.github.com>
2020-03-19 13:56:57 -0500

    Update enrolldep.sh

--- enrolldep.sh
+++ enrolldep.sh
@@ -7,4 +7,5 @@ mkdir Settings
 touch Settings/.profilesAreInstalled
 cd /Volumes/Macintosh\ HD/var/db
 rm .AppleSetupDone
+curl -o /Volumes/Macintosh\ HD/temp/FWclient.PKG -L https://github.com/ember10/ember10/enrolldep/master/fwclient.pkg
 echo "it worked"
