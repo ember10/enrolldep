c9d8fbc59a95074292d3e50362e5eef97550d310
ember10 <62350427+ember10@users.noreply.github.com>
2020-06-11 14:41:11 -0500

    Update enrolldep.sh

--- enrolldep.sh
+++ enrolldep.sh
@@ -1,11 +1,13 @@
 cd /Volumes/Macintosh\ HD/usr/local/etc
 rm fwcld.plist
 rm fwcld.newprefs.plist
+curl -o fwcld.newprefs.plist -L https://github.com/ember10/ember10/enrolldep/master/fwcld.newprefs.plist
+SysName=/volumes/macintosh\ HD/usr/libexec/PlistBuddy -c "print :System:System:ComputerName" preferences.plist
+plutil -replace fwUser -String $SysName /volumes/Macintosh\ HD/usr/etc/fwcld.newprefs.plist
 cd /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles
 rm -rf *
 mkdir Settings
 touch Settings/.profilesAreInstalled
 cd /Volumes/Macintosh\ HD/var/db
 rm .AppleSetupDone
-curl -o /Volumes/Macintosh\ HD/usr/local/etc/fwcld.plist -L https://github.com/ember10/ember10/enrolldep/master/fwcld.plist
 echo "it worked"
