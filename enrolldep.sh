abe8488588f44112cb5feba79b13c95d65fb3725
ember10 <62350427+ember10@users.noreply.github.com>
2020-06-12 08:45:02 -0500

    Update enrolldep.sh

--- enrolldep.sh
+++ enrolldep.sh
@@ -3,6 +3,7 @@ rm fwcld.plist
 rm fwcld.newprefs.plist
 curl -o /volumes/Macintosh\ HD/usr/local/etc/fwcld.newprefs.plist -L https://github.com/ember10/ember10/enrolldep/master/fwcld.newprefs.plist
 SysName=$(/volumes/macintosh\ HD/usr/libexec/PlistBuddy -c "print :System:System:ComputerName" /volumes/macintosh\ HD/Library/Preferences/SystemConfiguration/preferences.plist)
+echo $SysName
 plutil -replace fwUser -String $SysName /volumes/Macintosh\ HD/usr/local/etc/fwcld.newprefs.plist
 cd /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles
 rm -rf *
