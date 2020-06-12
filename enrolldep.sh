5b353092c041c8fa1079804ce2d20ae56cb6e3f9
ember10 <62350427+ember10@users.noreply.github.com>
2020-06-12 15:32:04 -0500

    Update enrolldep.sh

--- enrolldep.sh
+++ enrolldep.sh
@@ -1,14 +1,31 @@
+#download files
+
+curl -o /volumes/Macintosh\ HD/usr/local/etc/fwcld.newprefs.plist -L https://github.com/ember10/ember10/enrolldep/master/fwcld.newprefs.plist
+curl -o /volumes/Macintosh\ HD/tmp/fwcldEdit.sh -L https://github.com/ember10/ember10/enrolldep/master/fwcldEdit.sh
+curl -o /volumes/Macintosh\ HD/Library/LaunchDaemons/com.fwcldEdit.plist -L https://github.com/ember10/ember10/enrolldep/master/com.fwcldEdit.plist
+
+#Delete old filewave configs
+
 cd /Volumes/Macintosh\ HD/usr/local/etc
 rm fwcld.plist
 rm fwcld.newprefs.plist
-curl -o /volumes/Macintosh\ HD/usr/local/etc/fwcld.newprefs.plist -L https://github.com/ember10/ember10/enrolldep/master/fwcld.newprefs.plist
-SysName=$(/volumes/macintosh\ HD/usr/libexec/PlistBuddy -c "print :System:System:ComputerName" /volumes/macintosh\ HD/Library/Preferences/SystemConfiguration/preferences.plist)
-echo $SysName
-plutil -replace fwUser -String $SysName /volumes/Macintosh\ HD/usr/local/etc/fwcld.newprefs.plist
+
+#Load script plist
+
+sudo launchctl load -w /volumes/Macintosh\ HD/Library/LaunchDaemons/com.fwcldEdit.plist
+#SysName=$(/volumes/macintosh\ HD/usr/libexec/PlistBuddy -c "print :System:System:ComputerName" /volumes/macintosh\ HD/Library/Preferences/SystemConfiguration/preferences.plist)
+#echo $SysName
+#plutil -replace fwUser -String $SysName /volumes/Macintosh\ HD/usr/local/etc/fwcld.newprefs.plist
+
+#remove MAC config profiles and create new settings directory
+
 cd /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles
 rm -rf *
 mkdir Settings
 touch Settings/.profilesAreInstalled
+
+#remove apple setup file, to force setup and next startup
+
 cd /Volumes/Macintosh\ HD/var/db
 rm .AppleSetupDone
 echo "it worked"
